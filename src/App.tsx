617cd99465aff8b11330e3bccdb902bccb3d088a
loktar00 <loktar00@users.noreply.github.com>
2022-09-08 02:25:37 +0000

    deploy: cacbaf38c750ecde2f04b1c03d9c3fe1dd4ec892

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import TextOverflow from 'react-text-overflow';
+
+import './style.css';
+
+const App = () => (
+    <main className="main-container">
+        <h2>Default</h2>
+        <section>
+            <p className="text-content">
+                <TextOverflow text="this is some text that we want to truncate." />
+            </p>
+            <p>
+                <TextOverflow text="this is some text that we want to truncate. Change the size of the window to check the truncation, it should truncate at the end." />
+            </p>
+        </section>
+        <h2>Middile truncatation</h2>
+        <section>
+            <p className="text-content">
+                <TextOverflow text="this is some text that we want to truncate." truncatePosition="middle" />
+            </p>
+            <p>
+                <TextOverflow text="this is some text that we want to truncate. Change the size of the window to check the truncation, it should truncate in the center." truncatePosition="middle" />
+            </p>
+        </section>
+        <h2>Start after the first character</h2>
+        <section>
+            <p className="text-content">
+                <TextOverflow
+                    truncatePosition="middle"
+                    startPos={1}
+                    text="this is some text that we want to truncate. Change the size of the window to check the truncation, it should truncate in the center."
+                />
+            </p>
+            <p>
+                <TextOverflow
+                    truncatePosition="middle"
+                    startPos={1}
+                    text="this is some text that we want to truncate. Change the size of the window to check the truncation, it should truncate in the center."
+                />
+            </p>
+        </section>
+        <h2>Beginning of string truncate</h2>
+        <section>
+            <p className="text-content">
+                <TextOverflow text="this is some text that we want to truncate." truncatePosition="start" />
+            </p>
+            <p>
+                <TextOverflow text="this is some text that we want to truncate. Change the size of the window to check the truncation, it should truncate at the start." truncatePosition="start" />
+            </p>
+        </section>
+    </main>
+);
+
+export default App;
